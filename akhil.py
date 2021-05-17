b9b358078ff207f0287b265793aa3517c1f942d0
AKHIL0004 <84148456+AKHIL0004@users.noreply.github.com>
2021-05-17 18:02:35 +0530

    Add files via upload

---
+++ akhil.py
@@ -0,0 +1,4 @@
+#input radius of the circle : 1.1
+#the area of circle with radius 1.1 is 3.0813271108436504
+print("input radius of the circle : 1.1")
+print("the area of circle with radius 1.1 is 3.0813271108436504")
